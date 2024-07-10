1895e3fa86a064603731af8c0e2d54149b81c2d4
Muhammad Iqbal Saputra <145625070+muhammadiqbalsaputra@users.noreply.github.com>
2024-07-10 14:14:23 +0700

    modulSoal_3.py

--- UAS_MUHAMMADIQBALSAPUTRA_23090120/modulSoal_3.py
+++ UAS_MUHAMMADIQBALSAPUTRA_23090120/modulSoal_3.py
@@ -1,16 +1,26 @@
-class restaurantwow:
+class restaurantow:
     def __init__(self):
         self.pesanan = []
-    
-    def Enqueue(self,Masukanpesanan):
-        self.pesanan.append(Masukanpesanan)
-    
+
+    def Enqueue(self):
+        masukanPesanan = input('Masukkan pesanan: ')
+        self.pesanan.append(masukanPesanan)
+        print(f'Order: {masukanPesanan} ditambahkan')
+
     def Dequeue(self):
-        if len(self.pesanan):
-            self.pesanan.pop(0)
+        if len(self.pesanan) > 0:
+            pesananDihapus = self.pesanan.pop(0)
+            print(f'Order : {pesananDihapus} Dihapus')
         else:
-            return None
-    
+            print('Antrian Tidak Ada')
+
     def tampilan(self):
-        for index, NamaPesanan in enumerate(self):
-            print(f'{index},{NamaPesanan}')
+        print('='*30)
+        if len(self.pesanan) > 0:
+            print('======= Daftar Antrian =======')
+            for index,antrian in enumerate(self.pesanan):
+                print(f'{index+1}. {antrian}')
+        else:
+            print('Antrian Tidak Ada')
+        print('='*30)
+
